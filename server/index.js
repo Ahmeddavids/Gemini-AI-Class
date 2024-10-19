726d770fe992b90fd33b5478d63d062040997c95
ahmed isiaka <ahmeddavids6@gmail.com>
2024-10-19 21:06:37 +0100

    Configured multer to interact with image

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 require('dotenv').config();
 const express = require('express');
 const PORT = 4040;
 const cors = require('cors');
 const app = express();
+const fs = require('fs');
+const multer = require('multer');
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+app.use(express.json());
+app.use(cors('*'))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`App is listening to PORT: ${PORT}`)
